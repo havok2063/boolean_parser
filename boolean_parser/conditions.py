ba95db445435540980e0086d02741a76279d2dbe
Brian Cherinka <havok2063@hotmail.com>
2019-02-13 18:36:15 -0500

    beginning to add in sqlalchemy mixin for boolean conditions

--- boolean_parser/conditions.py
+++ boolean_parser/conditions.py
@@ -7,7 +7,7 @@
 # Created: Wednesday, 13th February 2019 1:27:16 pm
 # License: BSD 3-clause "New" or "Revised" License
 # Copyright (c) 2019 Brian Cherinka
-# Last Modified: Wednesday, 13th February 2019 3:43:20 pm
+# Last Modified: Wednesday, 13th February 2019 6:31:01 pm
 # Modified By: Brian Cherinka
 
 
@@ -25,17 +25,18 @@ class Word(BooleanClause):
     ''' Class to handle word clauses '''
     def __init__(self, data):
         super(Word, self).__init__(data)
-        self.word = self.data.get('word', None)
+        self.name = self.data.get('word', None)
 
     def __repr__(self):
-        return f'{self.word}'
+        return f'{self.name}'
 
 
 class Condition(BooleanClause):
     ''' Class to handle logical expression clauses '''
     def __init__(self, data):
-        super(Condition, self).__init__(data)    
-        self.name = self.data.get('parameter', None)
+        super(Condition, self).__init__(data) 
+
+        self._parse_parameter_name()
         self.operator = self.data.get('operator', None)
         self._extract_values()
 
@@ -43,44 +44,96 @@ class Condition(BooleanClause):
         more = 'and' + self.value2 if hasattr(self, 'value2') else ''
         return self.name + self.operator + self.value + more
 
+    def _parse_parameter_name(self):
+        ''' parse the parameter name into a base + name '''
+        self.fullname = self.data.get('parameter', None)
+        if '.' in self.fullname:
+            self.base, self.name = self.fullname.split('.', 1)
+        else:
+            self.base = None
+            self.name = self.fullname
+            
     def _extract_values(self):
         ''' Extract the value or values from the condition '''
         self.value = self.data.get('value', None)
         if not self.value:
             if self.operator == 'between':
-                self.value = self.data.get('value1')
-                self.value2 = self.data.get('value2')
+                self.value = self._check_bitwise_value(self.data.get('value1'))
+                self.value2 = self._check_bitwise_value(self.data.get('value2'))
+
+        self.value = self._check_bitwise_value(self.value)
+
+    def _check_bitwise_value(self, value):
+        ''' check if value has a bitwise ~ in it
+
+        Removes any bitwise ~ found in a value for a condition.
+        If the operand is a bitwise & or |, convert the ~value to its
+        integer appropriate.  E.g. ~64 -> -65.
+
+        Parameters:
+            value (str): A string numerical value
+
+        Returns:
+            The str value or value converted to the proper bitwise negative
+        '''
+
+        if '~' in value:
+            value = value.replace('~', '')
+            if self.operator in ['&', '|']:
+                value = str(-1 * (int(value)) - 1)
+
+        return value
 
 
-class BoolNot(object):
+class BaseBool(object):
+    def __init__(self):
+        self.params = {}
+        self.logicop = None
+
+    def _update_params(self, condition):
+        ''' update the parameters dictionary '''
+        if isinstance(condition, BooleanClause) and condition.name not in self.params:
+            self.params.update({condition.fullname: condition.value})
+
+    def _get_conditions(self, data):
+        ''' build the list of conditions '''
+        self.conditions = []
+        for condition in data:
+            if condition and condition != self.logicop:
+                self._update_params(condition)
+                self.conditions.append(condition)
+
+
+class BoolNot(BaseBool):
     ''' Base class for Boolean Not logic '''
     def __init__(self, data):
+        super(BoolNot, self).__init__()
+        self.logicop = 'not'
         self.condition = data[0][1]
+        self._update_params(self.condition)
 
     def __repr__(self):
         return f'not_({repr(self.condition)})'
 
 
-class BoolAnd(object):
+class BoolAnd(BaseBool):
     ''' Base class for Boolean And logic '''
     def __init__(self, data):
-        self.conditions = []
-        for condition in data[0]:
-            if condition != 'and':
-                self.conditions.append(condition)
+        super(BoolAnd, self).__init__()
+        self.logicop = 'and'
+        self._get_conditions(data[0])
 
     def __repr__(self):
         strcond = ', '.join([repr(c) for c in self.conditions])
         return f'and_({strcond})'
 
 
-class BoolOr(object):
+class BoolOr(BaseBool):
     ''' Base class for Boolean Or logic '''
     def __init__(self, data):
-        self.conditions = []
-        for condition in data[0]:
-            if condition != 'or':
-                self.conditions.append(condition)
+        super(BoolOr, self).__init__()
+        self.logicop = 'or'
+        self._get_conditions(data[0])
 
     def __repr__(self):
         strcond = ', '.join([repr(c) for c in self.conditions])
@@ -108,29 +161,6 @@ between_cond = pp.Group(name + pp.CaselessLiteral('between').setResultsName('ope
                         value.setResultsName('value1') + pp.CaselessLiteral('and') +
                         value.setResultsName('value2'))
 between_cond.setParseAction(Condition)
-
-# ------
-# define base parser for functions
-LPAR = pp.Suppress('(')
-RPAR = pp.Suppress(')')
-# list of numbers
-nl = pp.delimitedList(number, combine=True)
-narr = pp.Combine('[' + nl + ']')
-# function arguments
-arglist = pp.delimitedList(
-    number | (pp.Word(pp.alphanums + '-_') + pp.NotAny('=')) | narr)
-args = pp.Group(arglist).setResultsName('args')
-# function keyword arguments
-key = pp.Word(pp.alphas) + pp.Suppress('=')
-values = (number | pp.Word(pp.alphas))
-keyval = pp.dictOf(key, values)
-kwarglist = pp.delimitedList(keyval)
-kwargs = pp.Group(kwarglist).setResultsName('kwargs')
-# build generic function
-fxn_args = pp.Optional(args) + pp.Optional(kwargs)
-fxn_name = (pp.Word(pp.alphas)).setResultsName('fxn')
-fxn = pp.Group(fxn_name + LPAR + fxn_args + RPAR)
-# fxn condition
 
 
 # ------
--- boolean_parser/mixins/sqla.py
+++ boolean_parser/mixins/sqla.py
@@ -7,16 +7,21 @@
 # Created: Wednesday, 13th February 2019 3:49:07 pm
 # License: BSD 3-clause "New" or "Revised" License
 # Copyright (c) 2019 Brian Cherinka
-# Last Modified: Wednesday, 13th February 2019 3:49:25 pm
+# Last Modified: Wednesday, 13th February 2019 5:59:17 pm
 # Modified By: Brian Cherinka
 
 
 from __future__ import print_function, division, absolute_import
+import inspect
+import decimal
 from sqlalchemy import func, bindparam, text
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import or_, and_, not_, sqltypes, between
 from operator import le, ge, gt, lt, eq, ne
-from boolean_parser.conditions import Condition
+
+from boolean_parser import BooleanParserException
+from boolean_parser.conditions import Condition, BoolNot, BoolOr, BoolAnd
 
 opdict = {'<=': le, '>=': ge, '>': gt, '<': lt, '!=': ne, '==': eq, '=': eq}
 
@@ -24,7 +29,224 @@ opdict = {'<=': le, '>=': ge, '>': gt, '<': lt, '!=': ne, '==': eq, '=': eq}
 class SQLAMixin(object):
     ''' '''
 
+    def _check_models(self, classes):
+        ''' Check the input modelclass format
+        
+        Checks if input classes is a module of modelclasses, a list of modelclasses
+        or a single ModelClass and returns a list of ModelClass objects. 
+        
+        Parameters:
+            classes (object):
+                A ModelClass module, list of models, or single ModelClass
+
+        Returns:
+            A list of ModelClasses
+        '''
+
+        # an entire module of classes
+        if inspect.ismodule(classes):
+            # an entire module of classes
+            models = [i[1] for i in inspect.getmembers(
+                classes, inspect.isclass) if hasattr(i[1], '__tablename__')]
+        elif isinstance(classes, (list, tuple)):
+            # a list of ModelClasses
+            models = classes
+        else:
+            # assume a single ModelClass
+            models = [classes]
+
+        # check for proper modelclasses
+        allmeta = all([isinstance(m, DeclarativeMeta) for m in models])
+        assert allmeta is True, 'All input classes must be of type SQLAlchemy ModelClasses'
+
+        return models
+
+    def _get_field(self, modelclass, field_name, base_name=None):
+        ''' Return a SQLAlchemy attribute from a field name.
+
+        Checks that a given model contains the named field.         
+
+        Parameters:
+            modelclass (ModelClass):
+                A SQLAlchemy ModelClass
+            field_name (str):
+                The database field name
+            base_name (str):
+                The database table name
+        
+        Returns:
+            An SQLA instrumented attribute object
+        '''
+
+        field = None
+        # Handle hierarchical field names such as 'parent.name'
+        if base_name:
+            if base_name in modelclass.__tablename__:
+                field = getattr(modelclass, field_name, None)
+        else:
+            # Handle flat field names such as 'name'
+            field = getattr(modelclass, field_name, None)
+
+        return field
+    
+    def filter(self, modelclass):
+        ''' Return the condition as an SQLalchemy query condition
+        
+        Loops over all models and creates a filter condition for that model
+        given the input filter parameters.
+
+        Parameters:
+            modelclass (objects):
+                A set of ModelClasses to use in the filter condition
+
+        Returns:
+            A SQL query filter condition
+        '''
+ 
+        assert modelclass is not None, 'No input found'
+
+        condition = None
+        models = self._check_models(modelclass)
+
+        for model in models:
+            # get the SQLA instrumented attribute
+            field = self._get_field(model, self.name, base_name=self.base)
+
+            # if there is an attribute then break and use that model
+            if field and hasattr(field, 'type') and hasattr(field, 'ilike'):
+                break
+
+        # raise if no attribute found
+        if not field:
+            raise BooleanParserException(f'Table {model.__tablename__} does not have field {self.name}')
+
+        # produce the SQLA filter condition
+        condition = self._filter_one(model, field=field, condition=condition)
+
+        return condition
+
+    def _filter_one(self, model, field=None, condition=None):
+        ''' Create a single SQLAlchemy filter condition '''
+
+        # if no field present return the original condition
+        if not field:
+            return condition
+
+        # Prepare field and value
+        lower_field, lower_value, lower_value_2 = self._bind_and_lower_value(field)
+
+        return condition
+
+    def _bind_and_lower_value(self, field):
+        ''' Bind and lower the value based on field type'''
+
+        lower_value_2 = None
+
+        # get python field type
+        ftypes = [float, int, decimal.Decimal]
+        fieldtype = field.type.python_type
+
+        # format the values
+        value, lower_field = self._format_value(self.value, fieldtype, field)
+        if hasattr(self, 'value2'):
+            value2, lower_field = self._format_value(self.value2, fieldtype, field)
+
+        # bind the parameter value to the parameter name
+        boundvalue = bindparam(self.bindname, value)
+        lower_value = func.lower(boundvalue) if fieldtype not in ftypes else boundvalue
+        if hasattr(self, 'value2'):
+            self.bindname = '{0}_{1}'.format(self.fullname, 2)
+            boundvalue2 = bindparam(self.bindname, value2)
+            lower_value_2 = func.lower(boundvalue2) if fieldtype not in ftypes else boundvalue2
+
+        return lower_field, lower_value, lower_value_2
+
+    def _bind_parameter_names(self):
+        ''' Bind the parameters names to the values '''
+
+        if self.fullname not in params:
+            params.update({self.fullname: self.value})
+            self.bindname = self.fullname
+        else:
+            count = list(params.keys()).count(self.fullname)
+            self.bindname = '{0}_{1}'.format(self.fullname, count)
+            params.update({self.fullname: self.value})
+            
+    def _format_value(self, value, fieldtype, field):
+        ''' Formats the value based on the fieldtype
+        
+        Formats the value to proper numreical type and lowercases
+        the field for string fields.
+        
+        Parameters:
+            value (str):
+                The conditional value to format
+            fieldtype (object):
+                The python field type
+            field (SQLA attribute):
+                SQLA instrumented attribute
+
+        Returns:
+            The formatted value and lowercase field
+        '''
+
+        lower_field = field
+        if fieldtype == float or fieldtype == decimal.Decimal:
+            out_value = self._cast_value(value, datatype=float)
+        elif fieldtype == int:
+            out_value = self._cast_value(value, datatype=int)
+        else:
+            lower_field = func.lower(field)
+            out_value = value
+
+        return out_value, lower_field
+
+    def _cast_value(self, value, datatype=float):
+        ''' Cast a value to a specific Python type
+
+        Parameters:
+            value (int|float):
+                A numeric value to cast to a float or integer
+            datatype (object):
+                The numeric cast function.  Can be either float or int.
+        
+        Returns:
+            The value explicitly cast to an integer or float
+        '''
+
+        assert datatype in [float, int], 'datatype must be either float or int'
+
+        try:
+            out = datatype(value)
+        except (ValueError, SyntaxError):
+            raise BooleanParserException(f'Field {self.name} expects a {datatype.__name__} value.  Received {value} instead.')
+        else:
+            return out
+
 
 class SQLACondition(SQLAMixin, Condition):
     ''' '''
     pass
+
+
+class SQLANot(BoolNot):
+    ''' SQLalchemy class for Boolean Not '''
+
+    def filter(self, modelclass):
+        return not_(self.condition.filter(modelclass))
+
+
+class SQLAAnd(BoolAnd):
+    ''' SQLalchemy class for Boolean And '''
+
+    def filter(self, modelclass):
+        conditions = [condition.filter(modelclass) for condition in self.conditions]
+        return and_(*conditions) 
+
+
+class SQLAOr(BoolOr):
+    ''' SQLalchemy class for Boolean Or '''
+
+    def filter(self, modelclass):
+        conditions = [condition.filter(modelclass) for condition in self.conditions]
+        return or_(*conditions)
